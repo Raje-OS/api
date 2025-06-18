da0ccbf74b2fce89b0a4ee4e947d1835765dd382
loreleysarmiento <sarmientoloreley@gmail.com>
2025-06-18 03:49:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-server.use(middlewares);
-server.use(router);
-
+const app = express();
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let db = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+
+// Generar rutas CRUD para cada colección del db.json
+Object.keys(db).forEach((key) => {
+  // GET ALL
+  app.get(`/${key}`, (req, res) => res.json(db[key]));
+
+  // GET BY ID
+  app.get(`/${key}/:id`, (req, res) => {
+    const item = db[key].find(i => i.id == req.params.id);
+    item ? res.json(item) : res.sendStatus(404);
+  });
+
+  // POST
+  app.post(`/${key}`, (req, res) => {
+    const nuevo = req.body;
+    nuevo.id = key.slice(0, 2).toUpperCase() + Math.floor(Math.random() * 10000);
+    db[key].push(nuevo);
+    res.status(201).json(nuevo);
+  });
+
+  // PUT
+  app.put(`/${key}/:id`, (req, res) => {
+    const index = db[key].findIndex(i => i.id == req.params.id);
+    if (index !== -1) {
+      db[key][index] = { ...req.body, id: req.params.id };
+      res.json(db[key][index]);
+    } else res.sendStatus(404);
+  });
+
+  // DELETE
+  app.delete(`/${key}/:id`, (req, res) => {
+    const index = db[key].findIndex(i => i.id == req.params.id);
+    if (index !== -1) {
+      db[key].splice(index, 1);
+      res.sendStatus(204);
+    } else res.sendStatus(404);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`API RAJE corriendo en puerto ${PORT}`);
 });
